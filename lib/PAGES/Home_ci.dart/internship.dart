f6396f55cbac78d51887356beeb70b1452e19409
deepak-dee3 <deepakdscute@gmail.com>
2025-02-12 22:10:29 +0530

    Navigation button set up in container

--- lib/PAGES/Home_ci.dart/internship.dart
+++ lib/PAGES/Home_ci.dart/internship.dart
@@ -46,6 +46,7 @@ class InternshipsPage extends StatelessWidget {
                 subtitle: Text('Click to apply'),
                 trailing: Icon(Icons.arrow_forward_ios),
                 onTap: () {
+                 
                   // Navigate to internship application page or details
                 },
               ),
