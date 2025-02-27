df2b40d92992454b08bce85d10e8d0f01da1e57d
deepak-dee3 <deepakdscute@gmail.com>
2025-02-27 20:30:03 +0530

    Style has been changed

--- lib/PAGES/courses.dart
+++ lib/PAGES/courses.dart
@@ -62,7 +62,7 @@ return Scaffold(
                                   ),
                                 ),
                               ),
-                            ],
+                           ],
                           ),
                         ),
                         Padding(
